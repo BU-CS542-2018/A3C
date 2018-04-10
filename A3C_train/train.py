efcf3f23530022575408e308d86925ae951594c7
zsqiii <zhangspp199267@gmail.com>
2018-04-10 16:18:49 -0400

    Update train.py

--- A3C_train/train.py
+++ A3C_train/train.py
@@ -1,6 +1,6 @@
 #--------------------------------------------------------------------------------------------------------------------------------
 # CS 542 Machine Learning Project, Winter 2018, Boston University
-# Modified by: Siqi Zhang
+# Modified for the purpose of project
 # Original code by OpenAI
 # Description: This is the main function that contains: 1. command line argument parsing. 2. tensorflow worker initialization
 #--------------------------------------------------------------------------------------------------------------------------------
